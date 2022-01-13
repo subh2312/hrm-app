1c0d241671c4781be873cd127852b7b8933454e2
subh2312 <subhankarm2312@gmail.com>
2022-01-13 10:20:39 +0530

    first commit for hrm app

---
+++ hrm-app-backend/src/test/java/com/subhankar/HrmAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhankar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
